7b775d3f4e871edae90b671e3e5c53e1d394f5d9
IuliaBara <100848831+IuliaBara@users.noreply.github.com>
2022-03-03 17:20:30 +0200

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,2 +1,11 @@
-echo "$WORKSPACE"
+echo "${WORKSPACE}"
 echo "This is a message from a batch file"
+${WORKSPACE}
+%WORKSPACE%
+echo %WORKSPACE%
+${env.WORKSPACE}
+echo ${env.WORKSPACE}
+%env.WORKSPACE%
+echo %env.WORKSPACE%
+$WORKSPACE
+echo $WORKSPACE
